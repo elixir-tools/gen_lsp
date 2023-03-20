fe5ae38bc52b3b378248f4f77b041c6275f69ea1
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-03-20 00:02:36 -0400

    remove some stuff

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,5 @@
 # Used by "mix format"
 [
   import_deps: [:typed_struct],
-  inputs: [
-    "{mix,.formatter}.exs",
-    "{config,lib,test,}/**/*.{ex,exs}",
-    "examples/credo_ls.exs",
-    "codegen.exs"
-  ]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test,}/**/*.{ex,exs}"]
 ]
