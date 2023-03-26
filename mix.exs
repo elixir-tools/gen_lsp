3b9a8ea5d7fc2a02227648e611628eee931b2de9
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-03-26 16:54:18 -0400

    chore: bump v0.0.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GenLSP.MixProject do
     [
       app: :gen_lsp,
       description: "Library for creating language servers",
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
