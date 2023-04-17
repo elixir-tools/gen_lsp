116e5accfe08a92e4a69a54e563cae15506a4018
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-04-16 22:00:28 -0400

    chore: export locals_without_parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,13 +1,18 @@
 # Used by "mix format"
+locals = [
+  assert_result: 2,
+  assert_notification: 2,
+  assert_result: 3,
+  assert_notification: 3,
+  notify: 2,
+  request: 2
+]
+
 [
-  locals_without_parens: [
-    assert_result: 2,
-    assert_notification: 2,
-    assert_result: 3,
-    assert_notification: 3,
-    notify: 2,
-    request: 2
-  ],
+  locals_without_parens: locals,
   import_deps: [:typed_struct],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test,}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test,}/**/*.{ex,exs}"],
+  export: [
+    locals_without_parens: locals
+  ]
 ]
